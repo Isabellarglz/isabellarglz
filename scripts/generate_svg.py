40e4c3bb57dc70b75875d19d5f76e034f790f2b0
Isabella Rodríguez <roisabella@uninorte.edu.co>
2026-07-30 23:06:37 -0500

    Update generate_svg.py

--- scripts/generate_svg.py
+++ scripts/generate_svg.py
@@ -1,1 +1,15 @@
-print("Hello")
+import json
+from pathlib import Path
+
+
+# Ruta hasta la carpeta raíz del proyecto
+ROOT = Path(__file__).parent.parent
+
+# Ruta al archivo de configuración
+CONFIG_PATH = ROOT / "config" / "config.json"
+
+# Abre el archivo
+with open(CONFIG_PATH, "r", encoding="utf-8") as file:
+    config = json.load(file)
+
+print(config)
